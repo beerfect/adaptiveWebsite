6e8f72330ccfe6a54a3f2e19d4f17daa2031d9ab
beerfect <beerfect@yandex.ru>
2019-06-09 21:06:45 +0300

    Slider complete

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,6 @@ const menuButton = document.querySelector('.menu__button');
 const menuUl = document.querySelector('.menu__ul');
 const menuVisible = document.querySelector('.menu-visible');
 
-
-
 let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 let height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
 
@@ -17,7 +15,3 @@ function showMenu() {
         menuVisible.addEventListener('click',showMenu);
     }
 }
-
-// 823+
-// 482-822
-// 0 - 481
